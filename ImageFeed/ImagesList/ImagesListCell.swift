492332ba8281e47ae4cf14a09752ce563efd6237
Maksim Kargin <maksim.kargin@gmail.com>
2025-05-01 16:43:14 +0700

    Add first cell image, like button and date label

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Maksim Kargin on 30.04.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    // MARK: - Public Properties
+    static let reuseIdentifier = "ImagesListCell"
+    
+}
