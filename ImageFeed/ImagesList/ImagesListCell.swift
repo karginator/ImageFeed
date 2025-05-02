6cbc66cb95b6f842519c81825f92fc0d0a1544d6
Maksim Kargin <maksim.kargin@gmail.com>
2025-05-02 13:43:55 +0700

    Sprint 8, review. Add all image. Add dynamic height for cell.

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -9,7 +9,11 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     
+    // MARK: - IB Outlets
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    
     // MARK: - Public Properties
     static let reuseIdentifier = "ImagesListCell"
-    
 }
