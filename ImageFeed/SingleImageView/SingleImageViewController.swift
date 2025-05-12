5e37e9aaff1e77eaf3d4dead99c71e5bc8a669c9
Maksim Kargin <maksim.kargin@gmail.com>
2025-05-12 11:53:33 +0700

    Add SingleImageViewController.

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Maksim Kargin on 07.05.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet private weak var imageView: UIImageView!
+    
+}
