ac882a2a6520cfbf88b1e50cc83465f8a9bfb567
Maksim Kargin <maksim.kargin@gmail.com>
2025-05-15 11:25:55 +0700

    Sprint 9, review.

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -69,6 +69,6 @@ final class SingleImageViewController: UIViewController {
 // MARK: - UIScrollViewDelegate extension
 extension SingleImageViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return imageView
+        imageView
     }
 }
